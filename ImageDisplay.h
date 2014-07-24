44d82543f8e8a19b6551abf7a8c86e55e8b0d048
Fughilli <kbalke@ucla.edu>
2014-07-24 02:40:31 -0700

    Removed generic implementation bits from ImageDisplay

--- ImageDisplay.h
+++ ImageDisplay.h
@@ -7,14 +7,9 @@ class ImageDisplay : public DisplayInterface
 protected:
     Image* _i;
 
-    void triArea(const Triangle2d& tri, const G3D_Color& color);
-
 public:
     ImageDisplay(Image& renderTarget);
 
-    void drawTri(const Triangle2d& tri, const G3D_Color& color, bool filled = false);
-    void drawLine(const Line2d& line, const G3D_Color& color);
     void drawPoint(const Vector2d& point, const G3D_Color& color);
-    void drawBox(const Line2d& diag, const G3D_Color& color);
     void clear(const G3D_Color& color);
 };
